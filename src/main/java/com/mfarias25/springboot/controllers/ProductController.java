409a24b516e0fc0a30a39b8f03e8801c1be19875
Mateus Farias <mateusfariasdev@outlook.com>
2023-09-23 00:09:37 -0300

    created method update(PUT)

--- src/main/java/com/mfarias25/springboot/controllers/ProductController.java
+++ src/main/java/com/mfarias25/springboot/controllers/ProductController.java
@@ -41,6 +41,17 @@ public class ProductController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
         }
         return ResponseEntity.status(HttpStatus.OK).body(product0.get());
+    }
 
+    @PutMapping("/products/{id}")
+    public ResponseEntity<Object>updateProducts(@PathVariable (value = "id")UUID id,
+                                                @RequestBody @Valid ProductRecordDto productRecordDto) {
+        Optional<ProductModel> product0 = productRepository.findById(id);
+        if (product0.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+        var productModel = product0.get();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(productModel));
     }
 }
